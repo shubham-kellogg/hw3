bfe13019b345f73a77f32cea3f25edc0284cfd1e
Shubham Bajaj <shubham.bajaj@kellogg.northwestern.edu>
2024-02-20 05:46:29 +0000

    trial_attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'entries/new'
+  get 'entries/create'
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
+  get 'places/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
